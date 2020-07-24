f07556fc437b452a766eae584a734060039f6896
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-07-24 13:31:01 -0400

    I did my best, im still having trouble understanding prompts vs solving them

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -54,11 +54,12 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-function populationTotal(data){
-  let populationTotal = 0;
+function populationTotal(population){
+  
+  let populationTotal = population.reduce((sum,entry) => entry +sum, 0);
 }
 
-console.log(populationTotal);
+console.log(populationTotal(zooAnimals));
 
 
 // ==== Callbacks ====  
@@ -69,19 +70,24 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(cb, a, b){
+  return cb(a,b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (a, b)=> a+b;
+const multiply =( a,b)=> a*b;
+const greeting= (firstName, lastName)=>`Hello ${firstName}${lastName}, nice to meet you!`;
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
