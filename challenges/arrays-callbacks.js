3600cb859309c1160415e76ac613c3e78f898ce5
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-07-24 12:18:40 -0400

    my classes broke when i took them out of prototype.

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -30,6 +30,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames
+};
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,34 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+/* ===== Prototype Practice ===== */
 
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+*/
+Class CuboidMaker{
+    constructor(length, width, height){
+    this.length= length;
+    this.width= width;
+    this.height= height;
+  }
+  volume(){
+     return this.length * this.width * this.height;
+  }
+  surfaceArea(){
+    return
+  }
+  const Cuboid = new CuboidMaker();
+  Cuboid.length(4);
+  Cuboid.width(5);
+  Cuboid.height(5);
+}
+  // Test your volume and surfaceArea methods by uncommenting the logs below:
+   console.log(cuboid.volume()); //100
+   console.log(cuboid.surfaceArea()); //130
+  
+  
+  
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
